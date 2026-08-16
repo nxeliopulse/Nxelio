55befc3c4f280451426616cac956a4840e446f40
nxeliopulse <nxeliopulse@gmail.com>
2026-08-16 17:44:33 +0530

    Analytics

--- src/app/(app)/analytics/layout.tsx
+++ src/app/(app)/analytics/layout.tsx
@@ -1,18 +1,15 @@
-import { listFolders } from "@/lib/queries/analytics-folders";
 import { listDashboards } from "@/lib/queries/analytics-dashboards";
 import { listReports } from "@/lib/queries/analytics-reports";
 import { AnalyticsShell } from "@/components/analytics/analytics-shell";
 
 export default async function AnalyticsLayout({ children }: { children: React.ReactNode }) {
-  const [dashboardFolders, reportFolders, dashboards, reports] = await Promise.all([
-    listFolders("dashboard"),
-    listFolders("report"),
+  const [dashboards, reports] = await Promise.all([
     listDashboards(),
     listReports(),
   ]);
 
   return (
-    <AnalyticsShell dashboardFolders={dashboardFolders} reportFolders={reportFolders} dashboards={dashboards} reports={reports}>
+    <AnalyticsShell dashboards={dashboards} reports={reports}>
       {children}
     </AnalyticsShell>
   );
